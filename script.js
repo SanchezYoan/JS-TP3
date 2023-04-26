13f9c4599cf058d26d82c2510081fdd898a68a9c
SanchezYoan <yoan30470@hotmail.fr>
2023-04-26 12:00:03 +0200

    correction

--- script.js
+++ script.js
@@ -1,40 +1,33 @@
 // Créer un événement au scroll
 
-// Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
 
-// Faire apparaitre l'image de la partie improvise
 
-// Faire apparaitre la popup quand on est en bas du site
+// document.body.offsetHeight
 
-// Bonus : quand on clicke sur la popup elle disparait pour toujours
+let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight
 
-const navScroll = 20;
-const imgScroll = 200;
-const popUpScroll = 1000;
-
-const nav = document.querySelector("#navbar")
-const img = document.querySelector("#imgImprovise")
-const popUp = document.querySelector(".pop-up")
-const closePopUp = document.querySelector("#closeBtn")
+playOnce = true;
 
 window.addEventListener("scroll", () => {
-    if(window.scrollY > navScroll) {
-        nav.style.height = "50px";
+    if(window.scrollY > 50) {
+        navbar.style.height = "45px";
     } else {
-        nav.style.height = "90px";
+        navbar.style.height = "90px";
     };
     
-    if(window.scrollY > imgScroll) {
-        img.style.opacity = "1";
-        img.style.transform = "translateX(-100px)";
-    }
+    if(scrollValue > 0.45) {
+        imgImprovise.style.opacity = "1";
+        imgImprovise.style.transform = "none";
+    };
     
-    if(window.scrollY > popUpScroll) {
-        popUp.style.opacity = "1";
-        popUp.style.transform = "translate(0)"
-    }
+    if(scrollValue > 0.80 && playOnce) {
+        popup.style.opacity = "1";
+        popup.style.transform = "none";
+        playOnce = false;
+    };
 })
 
-closePopUp.addEventListener("click", () => {
-    popUp.remove()
-})
+closeBtn.addEventListener("click", () => {
+    popup.style.opacity = 0;
+    popup.style.transform = "translateX(500px)";
+});
